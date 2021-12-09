6f68ac3f84c28f8ef3542720ecb8b8a222a13b2c
FDKevin <fdkevinzero@gmail.com>
2021-12-09 15:51:55 +0800

    feat(internet): handle internet check error

--- pkg/srun/internet.go
+++ pkg/srun/internet.go
@@ -2,18 +2,23 @@ package srun
 
 import (
 	"github.com/parnurzeal/gorequest"
+	"log"
 	"net/http"
 	"net/url"
 )
 
 // Internet 通过Http请求是否被302到 srun登陆地址 来判断是否能访问互联网
 func (s PortalServer) Internet() bool {
-	redirected := false
+	connect := true
 	reqUrl, _ := url.ParseRequestURI(s.internetCheck)
-	gorequest.New().Get(reqUrl.String()).
+	_, _, errs := gorequest.New().Get(reqUrl.String()).
 		RedirectPolicy(func(req gorequest.Request, via []gorequest.Request) error {
-			redirected = req.URL.Hostname() == reqUrl.Hostname()
+			connect = req.URL.Hostname() != reqUrl.Hostname()
 			return http.ErrUseLastResponse
 		}).End()
-	return redirected
+	if errs != nil || len(errs) != 0 {
+		log.Printf("Internet check failed: %v", errs)
+		connect = false
+	}
+	return connect
 }
